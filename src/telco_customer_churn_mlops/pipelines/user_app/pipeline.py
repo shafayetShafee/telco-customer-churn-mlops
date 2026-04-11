08566adde076f5bda7bf9c26e24346babb577fe7
Shafayet Khan Shafee <sshafee@isrt.ac.bd>
2026-04-12 01:33:23 +0600

    draft: added user app scaffold

---
+++ src/telco_customer_churn_mlops/pipelines/user_app/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'user_app'
+generated using Kedro 1.2.0
+"""
+
+from kedro.pipeline import Node, Pipeline  # noqa
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([])
